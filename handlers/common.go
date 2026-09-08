9c11c829b116c31d665bd4cf594deb736ebf5e44
Genre Mamanao <41022228+glenngenre@users.noreply.github.com>
2026-09-08 14:21:56 +0800

    feat: add copyResponse function in common.go

--- handlers/common.go
+++ handlers/common.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
@@ -13,4 +14,14 @@ func writeError(w http.ResponseWriter, status int, msg string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(errorResponse{Error: msg})
+}
+
+func copyResponse(w http.ResponseWriter, resp *http.Response) {
+	for k, vv := range resp.Header {
+		for _, v := range vv {
+			w.Header().Add(k, v)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
